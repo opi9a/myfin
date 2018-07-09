80ab6df1db519ff883360c9dbf38c7d0fb3ee16c
gav <opi9a@gavinroberts.com>
2018-07-09 11:31:30 +0100

    finished refactoring categorise(), and defining flow based on unknowns.  Not yet completed "update_categs()"

--- finance/categorise.py
+++ finance/categorise.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from fuzzywuzzy import fuzz, process
 
-def categorise(items, txdb, account, fuzzymatch=True, fuzzy_threshold=80):
+    
+def categorise(items, account, txdb=None, cat_db=None, fuzzymatch=True, fuzzy_threshold=80):
     """
     - take iterable of items - eg column of new_tx df
     - iterate over items (df.apply is not faster), generating matches
@@ -12,21 +13,91 @@ def categorise(items, txdb, account, fuzzymatch=True, fuzzy_threshold=80):
     - returns two lists, for the matched categories and the ids
 
     """
-    # organise txdb for lookup: by item, strip() lower(),  only reqd columns
-    lookup_df = (txdb.set_index(txdb['item'].str.lower(), drop=True)
-                 .sort_index()[['accY','accX','id','mode']])
+    if txdb is None and cat_db is None:
+        print("need a txdb and/or a cat_db")
+        return 1
 
-    # drop unknowns
-    lookup_df = lookup_df.loc[lookup_df['accY'] != 'unknown']
+    # organise txdb for lookup by item (lower, stripped, no 'unknown's)
+    txdb_by_item = itemise(txdb)[['accY','accX','id','mode']]
 
     # get results - a list of tuples
     results = []
     for item in items:
-        results.append(get_category(item, lookup_df, account,
-                                    fuzzymatch, fuzzy_threshold))
 
-    return [x[0] for x in results], [x[1] for x in results] 
+        item = item.lower().strip()
 
+        if txdb_by_item.index.contains(item):
+            item_subdf = txdb_by_item.loc[[item],:]
+
+            # 1. try assigned hits in txdb
+            assigned_hits = item_subdf.loc[item_subdf['mode'] == -1]
+            if len(assigned_hits) > 0:
+                results.append(pick_match(item, account, assigned_hits))
+                continue
+
+            # 2. try unassigned hits in txdb
+            unassigned_hits = item_subdf.loc[item_subdf['mode'] > -1]
+            if len(unassigned_hits) > 0:
+                results.append(pick_match(item, account, unassigned_hits))
+                continue
+
+        # 3. try fuzzy match - test against whole index
+        if fuzzymatch:
+            best_match, score = process.extractOne(item, txdb_by_item.index,
+                                                   scorer=fuzz.token_set_ratio)
+
+            # just return the first - so works if single or multiple
+            if score >= fuzzy_threshold:
+                results.append((txdb_by_item.loc[[best_match],'accY'].iloc[0],
+                                txdb_by_item.loc[[best_match],'id'].iloc[0]))
+                continue
+
+        # 4. if nothing's returned by now, there's no match
+        results.append(('unknown', 0))
+
+    # return [x[0] for x in results], [x[1] for x in results] 
+    return results
+
+
+def itemise(txdb, drop_unknowns=True):
+    """Reorganises a tx dataframe to support querying for category by item:
+        - apply strip() and lower() to 'item' field
+        - set 'item' as index
+
+    Returns all columns (doesn't apply a selection, eg for just
+                         'accY', 'accX', 'id', 'mode')
+    """
+    
+    itemised = txdb.set_index(txdb['item'].str.lower(), drop=False)
+
+    if drop_unknowns:
+        itemised = itemised.loc[itemised['accY'] != 'unknown'].drop('item', axis=1)
+
+    return itemised
+
+
+def pick_match(item, account, sub_df):
+    """Returns match for item in sub_df of matches, giving preference for hits
+    in home account
+    """
+    # if only one match, return it
+    if len(sub_df) == 1:
+        return (sub_df.loc[item,'accY'],
+                sub_df.loc[item,'id'])
+    
+    # if more than one, look for a hit in home account
+    if len(sub_df) > 1:
+        home_acc_hits = sub_df[sub_df['accX']==account]
+
+        # if any home hits, return the first - works even if multiple
+        if len(home_acc_hits) > 0:
+            return (home_acc_hits.iloc[0].loc['accY'], 
+                    home_acc_hits.iloc[0].loc['id'])
+
+        # if no home account hits, just return the first assigned hit
+        else:
+            return (sub_df.iloc[0].loc['accY'], 
+                    sub_df.iloc[0].loc['id'])
 
 
 def get_category(item, tx_by_item, account=None, fuzzymatch=True, fuzzy_threshold=80):
@@ -52,6 +123,8 @@ def get_category(item, tx_by_item, account=None, fuzzymatch=True, fuzzy_threshol
     # - better done in preparing the input argument to this func, 
     # which will be used many times
     tx_by_item = tx_by_item.loc[tx_by_item['accY'] != 'unknown']
+
+    return tx_by_item
 
     if tx_by_item.index.contains(item):
         item_df = tx_by_item.loc[[item],:]
@@ -116,4 +189,19 @@ def get_category(item, tx_by_item, account=None, fuzzymatch=True, fuzzy_threshol
     return 'unknown', 0
 
 
-
+def recategorise(txdb_path):
+    # load txdb, and select unknowns
+    txdb = pd.read_csv(txdb_path, index_col='date')
+    
+    # from that, get a list of items
+    items = txdb.loc[txdb['accY']=='unknown', 'item']
+    
+    # use items to return hits and modes for the unknowns
+    hits, modes = categorise(items, txdb, 'halifax_cc')
+    
+    # over-write the txdb - modes first, as after hits they aren't 'unknown'
+    txdb.loc[txdb['accY']=='unknown', 'mode'] = modes
+    txdb.loc[txdb['accY']=='unknown', 'accY'] = hits
+    
+    # write out
+    txdb.to_csv('txdb.csv')
--- finance/load_new_txs.py
+++ finance/load_new_txs.py
@@ -2,22 +2,19 @@ import pandas as pd
 import numpy as np
 import os
 
-from finance.categorise import categorise
+from finance.categorise import categorise, itemise
 from finance.general import consol_debit_credit
 
-def load_new_txs(raw_tx_path, txdb_file=None,
+def load_new_txs(new_tx_paths, txdb_path=None, unknowns_path=None,
                  account_name=None, parser=None,
                  return_df=False):
+
     """Import raw transactions and return a tx_df in standard format,
     with date index, and columns: from, to, amt
 
-    TODO:
-        - add unique ID field
-        - categorise from and to cols for 'from_to' type
-
-    raw_tx_path  : the path of the raw transaction csv files, or list of files
+    new_tx_paths : a list of csv files with new transactions
     
-    tx_db_file   : the path of the existing tx database. 
+    txdb_path:     the path of the existing tx database. 
                    (or a new one to create)
 
     account_name : name of the account, for assignation in the 'from' and
@@ -35,73 +32,73 @@ def load_new_txs(raw_tx_path, txdb_file=None,
 
     """
 
-    # first, if a list of files has been passed, then simply run the function on each
-    if isinstance(raw_tx_path, list):
-        for f in raw_tx_path:
-            print('loading file', f)
-            load_new_txs(f, txdb_file, account_name, parser, return_df)
-            return
-
-    print('loading file(2)', raw_tx_path)
-    print('loading file(2)', os.path.abspath(raw_tx_path))
-    if account_name is None: account_name = 'No acc given'
-
-    if txdb_file is None: txdb_file = 'new_txdb.csv'
-
-    if parser is None: 
-        print('Need a parser')
-        return
-
+    # 1. aggregate input csvs to a single df
     date_parser = lambda x: pd.datetime.strptime(x, parser['date_format'])
+    raw_df = pd.concat([pd.read_csv(f, parse_dates=[parser['mappings']['date']],
+                                    skipinitialspace=True,
+                                    date_parser=date_parser, dayfirst=True)
+                                    for f in new_tx_paths])
 
-    raw_df = pd.read_csv(raw_tx_path, parse_dates=[parser['mappings']['date']],
-                         skipinitialspace=True,
-                         date_parser=date_parser, dayfirst=True)
-
-    # select only the columns reqd
+    # 2. organise columns using parser
     raw_df = raw_df[list(parser['mappings'].values())]
-
-    # map them to the standard labels
     raw_df.columns = parser['mappings'].keys()
 
-    return raw_df
+    # for credit_debits, make a 'net_amt' column
+    if parser['input_type'] == 'credit_debit':
+        raw_df['net_amt'] = (raw_df['debit_amt']
+                             .subtract(raw_df['credit_amt'], fill_value=0))
+        
+    # 3. read in txdb from csv
+    # TODO - handle case if file absent
+    txdb = pd.read_csv(txdb_path, index_col='date')
 
-    # unless already in 'from_to', do the conversion
-    if parser['input_type'] != 'from_to':
+    # 4. call categorise() to get accY and mode columns,
+    categ_out = categorise(raw_df['item'], account=account_name, txdb=txdb)
+    raw_df['accY'] = [x[0] for x in categ_out]
+    raw_df['mode'] = [x[1] for x in categ_out]
 
-        # first, from 'credit_debit' to 'net_amt'
-        if parser['input_type'] == 'credit_debit':
-            raw_df['net_amt'] = (raw_df['debit_amt']
-                                 .subtract(raw_df['credit_amt'], fill_value=0))
+    # 5. add id and account name columns to make output df
+    max_current = int(pd.read_csv(txdb_path)['id'].max())
+    raw_df['id'] = np.arange(max_current + 1,
+                              max_current + 1 + len(raw_df)).astype(int)
 
-        # then to 'from_to', first assigning categories
-        raw_df['category'] = categorise(raw_df['item'], categ_map)
+    raw_df['accX'] = account_name
 
-        raw_df[['from', 'to', 'item_from_to']] = \
-            consol_debit_credit(raw_df, account_name)
-
-    # clean up, get rid of negative amounts and select/order desired columns
-    raw_df['amt'] = raw_df['net_amt'].abs()
-
-    df_out = raw_df[['date', 'from', 'to', 'amt', 'item', 'item_from_to']].set_index('date')
-
-
-    # get max of current
-    max_current = 0
-    try:
-        max_current = int(pd.read_csv(txdb_file)['uid'].max())
-    except: pass
-
-    df_out['uid'] = np.arange(max_current + 1,
-                              max_current + 1 + len(df_out)).astype(int)
+    df_out = (raw_df[['date', 'item', 'accX', 'accY', 'net_amt', 'mode',
+                      'id']] .set_index('date'))
     
-    # handle if txdb file not already existing
-    if os.path.isfile(txdb_file):
-        df_out.to_csv(txdb_file, mode="a", header=False, date_format="%d/%m/%Y")
+    # 6. append to csv on disk - handle if txdb file not already existing
+    if os.path.isfile(txdb_path):
+        df_out.to_csv(txdb_path, mode="a", header=False, date_format="%d/%m/%Y")
     else:
-        df_out.to_csv(txdb_file, mode="w", header=True, date_format="%d/%m/%Y")
+        df_out.to_csv(txdb_path, mode="w", header=True, date_format="%d/%m/%Y")
+
+
+    # 7. write out unknowns to unknowns.csv
+    unknowns_df = df_out.loc[df_out['accY'] == 'unknown']
+    unknowns_df = itemise(unknowns_df, drop_unknowns=False)
+    unknowns_df = unknowns_df[['accX','accY']]
+
+    if os.path.isfile(unknowns_path):
+        unknowns_df.to_csv(unknowns_path, mode="a", header=False)
+    else:
+        unknowns_df.to_csv(unknowns_path, mode="w", header=True)
 
     if return_df: return df_out
 
 
+def update_category(item, accX, old_accY, new_accY, txdf):
+    """Update the assignation of accY category in txdf for rows labelled item:
+        - find rows of item associated with accX (or all, if accX is none),
+          with old_accY as accY (or all if old_accY is none)
+        - overwrite accY with new_accY 
+
+        Remember the input item will be lower()ed and stripp()ed
+    """
+    # 1. get the txdf selection
+    mask =    (txdf['item'].str.lower().str.strip() == item) \
+            & (txdf['accX'] == accX)                         \
+            & (txdf['accY'] == old_accY)
+
+    select_df = txdf.loc[mask]
 
